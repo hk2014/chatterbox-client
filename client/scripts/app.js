fac448e73c0af4e785ea46ff7cd4450857ace2bc
shawnhr <shawnk0818@gmail.com>
2017-10-20 20:11:43 -0700

    add working implementation

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,85 +8,87 @@ let app = {
   
 };
 
-app.init = function() {
-  
-  let start = function() {
+$('document').ready(function() {
+
+  app.init = function() {
     app.fetch();
+    
   };
 
-  
-  $('document').ready(function() {
 
-    $('.username').on('click', app.handleUsernameClick);
+  app.send = function(message) {
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  };
 
-    $('#send .submit').submit(app.handleSubmit);
-    
+  app.fetch = function() {
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        _.each(data.results, data => {
+          app.messages.push(data);
+          app.renderMessage(data);
+        });
+        //console.log('success', data.results);
+      }
+    });
+  };
+
+  app.clearMessages = function() {
+    $('#chats').html('');
+  };
+
+  app.renderMessage = function(messageData, user, text) {
+    messageData = messageData || {};
+    $('#chats').
+      append(
+        `<div id='message'>
+          <span class='username'>${user || messageData.username}</span>
+          <p class='text'>${text || messageData.text}</p>
+        </div>`);  
+  };
+
+  app.renderRoom = function(roomName) {
+    $('#roomSelect').append(`<div>${roomName}</div>`);
+  };
+
+  app.handleUsernameClick = function(user) {
+    app.friends.push(user);
+    console.log('username: ', user.currentTarget.childNodes["0"].data);
+  };
+
+  app.handleSubmit = function() {
+    console.log('submit');
+    app.renderMessage(undefined, app.user, $('#message').val);
+  };
+
+
+  app.init();
+
+  $('#chats').on('click',
+    '.username',
+    {user: $('#chats .username').currentTarget},
+    app.handleUsernameClick
+  );
+
+
+  $('#send .submit').submit(() => {
+    app.handleSubmit;
   });
   
-  start();
-  
-};
-
-
-app.send = function(message) {
-  $.ajax({
-    url: this.server,
-    type: 'POST',
-    data: message,
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
-};
-
-app.fetch = function() {
-  $.ajax({
-    url: this.server,
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      _.each(data.results, data => {
-        app.messages.push(data);
-        app.renderMessage(data);
-      });
-      //console.log('success', data.results);
-    }
-  });
-};
-
-app.clearMessages = function() {
-  $('#chats').html('');
-};
-
-app.renderMessage = function(messageData, user, text) {
-  messageData = messageData || {};
-  $('#chats').
-    append(
-      `<div id='message'>
-        <button class='username'>${user || messageData.username}</button>
-        <p class='text'>${text || messageData.text}</p>
-      </div>`);  
-};
-
-app.renderRoom = function(roomName) {
-  $('#roomSelect').append(`<div>${roomName}</div>`);
-};
-
-app.handleUsernameClick = function() {
-  console.log('something');
-};
-
-app.handleSubmit = function() {
-  console.log('kjasdflkashdf')
-  app.renderMessage(undefined, app.user, $('#message').val);
-};
-
-
-app.init();
+});
 
 
 
